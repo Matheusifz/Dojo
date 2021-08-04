08d62334d9b55c9ed5ffb26e6ab68688a0aeda4f
matheusifz <matheusifz@gmail.com>
2021-08-04 17:10:06 -0300

    feat: add error message if data not found

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-const BlogList = (props) => {
-  const blogs = props.blogs;
-  const title = props.title;
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
+
+const BlogList = ({ blogs, title }) => {
+  const titleFormated = titleCase(title);
 
   return (
     <div className="blog-list">
-      <h2>{title}</h2>
+      <h2>{titleFormated}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
